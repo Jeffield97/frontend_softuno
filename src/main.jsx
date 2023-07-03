68f206e771a2400e38431ed7694f19597b063f51
Jeffield97 <jchariguaman@gmail.com>
2023-07-02 21:55:22 -0500

    Configuracion Tailwind

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router/index.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Provider>
+  </React.StrictMode>
+);
